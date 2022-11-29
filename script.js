bd2a8747574a5f3bf3528f9ee3e9c1f6bac15361
Andrii Litviniuk <litandsh@gmail.com>
2022-11-29 10:09:17 +0100

    feat: implement functonality 'next' button

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const imgCollection = [
 for (let i = 0; i < imgCollection.length; i++) {
   document.querySelector('.slider').innerHTML += `<div class="thumb"><img src=${
     imgCollection[i]
-  } class="imgStyle"  alt="image_0${i + 1}"></div>`;
+  } class="imgStyle active-img"  alt="image_0${i + 1}"></div>`;
 }
 
 // Create and add div container for image max size
@@ -19,6 +19,7 @@ divBigImage.classList.add('max-size');
 // Create and add img for image max size
 const bigImage = document.createElement('img');
 bigImage.src = imgCollection[2];
+bigImage.classList.add('img-max');
 bigImage.alt = 'big-picture';
 divBigImage.appendChild(bigImage);
 document.querySelector('.slider').before(divBigImage);
@@ -43,5 +44,32 @@ buttonNext.classList.add('btnStyle', 'next');
 buttonNext.textContent = 'Next';
 buttonContainer.appendChild(buttonNext);
 
-
+//======================================
 const images = document.querySelectorAll('.imgStyle');
+let count = 0; //Number of current image
+
+// Button functionality
+const next = document.querySelector('.next');
+next.onclick = nextFunction;
+
+// const prev = document.querySelector('.prev');
+// prev.onclick = prevFunction;
+
+// const reset = document.querySelector('.reset');
+// reset.onclick = resetFunction;
+
+// Button's functions
+function nextFunction() {
+    if (count + 1 < images.length) {
+        count++;
+    } else {
+        count = 0
+    }
+
+    // Delete active-img class
+    for (let i = 0; i < images.length; i++) {
+        images[i].classList.remove('active-img');
+    }
+    images[count].classList.add('active-img');
+    document.querySelector('.img-max').src = images[count].src;
+}
